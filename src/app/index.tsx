8fe3446f6c36dd3d7374b918685a7bbebfd02bb6
emerss001 <emersonnevess80@gmail.com>
2025-04-06 20:52:55 -0300

    configurações inciais do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-green-500 ">
+            <Text className="text-2xl">Hello World!!!!</Text>
+        </View>
+    );
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    background: "#FFFFFF",
+
+    gray: {
+        200: "#E4E4E7",
+        300: "#D4D4D8",
+    },
+
+    green: {
+        200: "#BBF7D0",
+        300: "#86EFAC",
+        400: "#4ADE80",
+    },
+
+    orange: {
+        200: "#FFEDD5",
+        300: "#FDDAA2",
+        400: "#FBBF24",
+    },
+};
