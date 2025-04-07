2921ad349a2be2af723420747cc9b535ace00534
emerss001 <emersonnevess80@gmail.com>
2025-04-07 10:26:27 -0300

    Atualiza paleta de cores do projeto

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,20 +1,39 @@
 export const colors = {
-    background: "#FFFFFF",
+    background: "hsl(0 0% 100%)",
+    foreground: "hsl(0 0% 20%)",
 
-    gray: {
-        200: "#E4E4E7",
-        300: "#D4D4D8",
+    card: "hsl(0 0% 100%)",
+    cardForeground: "hsl(0 0% 20%)",
+
+    popover: "hsl(0 0% 100%)",
+    popoverForeground: "hsl(0 0% 20%)",
+
+    primary: "hsl(42 100% 50%)",
+    primaryForeground: "hsl(0 0% 98%)",
+
+    secondary: "hsl(0 0% 96.1%)",
+    secondaryForeground: "hsl(0 0% 20%)",
+
+    muted: "hsl(0 0% 96.1%)",
+    mutedForeground: "hsl(0 0% 45.1%)",
+
+    accent: "hsl(0 0% 96.1%)",
+    accentForeground: "hsl(0 0% 20%)",
+
+    destructive: "hsl(0 84.2% 60.2%)",
+    destructiveForeground: "hsl(0 0% 98%)",
+
+    border: "hsl(198 33% 94%)",
+    input: "hsl(198 33% 94%)",
+    ring: "hsl(0 0% 20%)",
+
+    chart: {
+        1: "hsl(12 76% 61%)",
+        2: "hsl(173 58% 39%)",
+        3: "hsl(197 37% 24%)",
+        4: "hsl(43 74% 66%)",
+        5: "hsl(27 87% 67%)",
     },
 
-    green: {
-        200: "#BBF7D0",
-        300: "#86EFAC",
-        400: "#4ADE80",
-    },
-
-    orange: {
-        200: "#FFEDD5",
-        300: "#FDDAA2",
-        400: "#FBBF24",
-    },
+    radius: "0.5rem", // esse valor pode ser convertido pra número se quiser
 };
