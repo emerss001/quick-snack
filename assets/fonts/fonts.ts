8fa8d3dbc86ecdc240c823ed81b053d9b28943fe
emerss001 <emersonnevess80@gmail.com>
2025-04-07 10:14:05 -0300

    Adiciona fontes Poppins e ícone da logo

---
+++ assets/fonts/fonts.ts
@@ -0,0 +1,11 @@
+// assets/fonts/fonts.ts
+
+export const FontAssets = {
+    "Poppins-Regular": require("./Poppins/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("./Poppins/Poppins-Bold.ttf"),
+};
+
+export const Fonts = {
+    Regular: "Poppins-Regular",
+    Bold: "Poppins-Bold",
+};
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,28 @@
-import { View, Text } from "react-native";
+import { FontAssets, Fonts } from "@/assets/fonts/fonts";
+import { View, Text, Image } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+    const [fontsLoaded] = useFonts(FontAssets);
+
+    if (!fontsLoaded) {
+        return null;
+    }
     return (
-        <View className="flex-1 items-center justify-center bg-green-500 ">
-            <Text className="text-2xl">Hello World!!!!</Text>
+        <View className="bg-background">
+            <View className="items-center justify-center gap-2 mt-20">
+                <Image source={require("@/assets/icons/Logo.png")} style={{ height: 82, width: 82 }} />
+                <Text className="font-bold text-xl">Quick Snack</Text>
+            </View>
+            <View className="px-6 mt-10 items-center">
+                <Text className="text-4xl font-semibold" style={{ fontFamily: Fonts.Regular }}>
+                    Seja bem-vindo
+                </Text>
+                <Text className="text-base text-center mt-2 max-w-[325]" style={{ fontFamily: Fonts.Regular }}>
+                    Escolha como prefere aproveitar sua refeição. Estamos aqui para oferecer praticidade e sabor em cada
+                    detalhe!
+                </Text>
+            </View>
         </View>
     );
 }
