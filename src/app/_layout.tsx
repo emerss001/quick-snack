df56638b7145b3426aa25f0006f3a94385d760f6
emerss001 <emersonnevess80@gmail.com>
2025-04-11 23:05:32 -0300

    adicionando configurações do react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,26 @@
 import { Slot } from "expo-router";
 import { SafeAreaView, StatusBar } from "react-native";
 import { colors } from "../styles/colors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 2,
+            staleTime: 1000 * 60 * 8, // 8 minutes
+            refetchOnWindowFocus: false,
+            gcTime: 1000 * 60 * 30, // 30 minutos
+        },
+    },
+});
 
 export default function RootLayout() {
     return (
-        <SafeAreaView className="bg-background">
-            <StatusBar backgroundColor={colors.background} />
-            <Slot />
-        </SafeAreaView>
+        <QueryClientProvider client={queryClient}>
+            <SafeAreaView className="bg-background">
+                <StatusBar backgroundColor={colors.background} />
+                <Slot />
+            </SafeAreaView>
+        </QueryClientProvider>
     );
 }
